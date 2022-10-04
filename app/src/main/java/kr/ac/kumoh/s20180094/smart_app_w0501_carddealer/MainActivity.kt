31154cdf21289be5ec1af694b9997f71c6ddb13e
agfalcon <agfalcon12@gmail.com>
2022-10-04 15:49:48 +0900

    using when!

--- app/src/main/java/kr/ac/kumoh/s20180094/smart_app_w0501_carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20180094/smart_app_w0501_carddealer/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.ac.kumoh.s20180094.smart_app_w0501_carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kr.ac.kumoh.s20180094.smart_app_w0501_carddealer.databinding.ActivityMainBinding
 
 
@@ -15,6 +16,27 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        getCardName(29)
         binding.card1.setImageResource(R.drawable.c_3_of_diamonds)
+    }
+
+    private fun getCardName(c: Int): String{
+        val shape = when (c/13) {
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else -> "error"
+        }
+        val number= when(c%13){
+            0 -> "ace"
+            in 1..9 -> ((c%13)+1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> "error"
+        }
+
+        return "c_${number}_of_${shape}"
     }
 }
