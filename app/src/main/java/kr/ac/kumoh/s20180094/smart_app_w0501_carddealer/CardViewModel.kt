a382f3d37e87f3c77fc707acc9c6d643c1a1160f
agfalcon <agfalcon12@gmail.com>
2022-10-11 15:49:20 +0900

    2022.10.11 project improved2⚡️

---
+++ app/src/main/java/kr/ac/kumoh/s20180094/smart_app_w0501_carddealer/CardViewModel.kt
@@ -0,0 +1,22 @@
+package kr.ac.kumoh.s20180094.smart_app_w0501_carddealer
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlin.random.Random
+
+class CardViewModel : ViewModel(){
+    private var _cards = MutableLiveData<IntArray>()
+    val cards: LiveData<IntArray>
+        get() = _cards
+
+    //val random = Random(System.currentTimeMillis())
+    val random = Random(100)
+    fun deal(){
+        var c = IntArray(5)
+        for(i in c.indices){
+            c[i] = random.nextInt(52)
+            _cards.value = c
+        }
+    }
+}
--- app/src/main/java/kr/ac/kumoh/s20180094/smart_app_w0501_carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20180094/smart_app_w0501_carddealer/MainActivity.kt
@@ -3,6 +3,8 @@ package kr.ac.kumoh.s20180094.smart_app_w0501_carddealer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import kr.ac.kumoh.s20180094.smart_app_w0501_carddealer.databinding.ActivityMainBinding
 import kotlin.random.Random
 import kotlin.random.Random.Default.nextInt
@@ -11,6 +13,7 @@ import kotlin.random.Random.Default.nextInt
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private val model: CardViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,13 +21,11 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
-        //binding.card1.setImageResource(R.drawable.c_3_of_diamonds)
-
-        binding.btnCard.setOnClickListener {
+        model.cards.observe(this, Observer<IntArray> {
             val res = IntArray(5)
-            for(i in 0 until res.size){
+            for (i in it.indices) {
                 res[i] = resources.getIdentifier(
-                    getCardName(Random.nextInt(52)),
+                    getCardName(it[i]),
                     "drawable",
                     packageName
                 )
@@ -34,7 +35,28 @@ class MainActivity : AppCompatActivity() {
             binding.card3.setImageResource(res[2])
             binding.card4.setImageResource(res[3])
             binding.card5.setImageResource(res[4])
+        })
+
+        binding.btnCard.setOnClickListener {
+            model.deal()
         }
+        //binding.card1.setImageResource(R.drawable.c_3_of_diamonds)
+
+//        binding.btnCard.setOnClickListener {
+//            val res = IntArray(5)
+//            for(i in 0 until res.size){
+//                res[i] = resources.getIdentifier(
+//                    getCardName(Random.nextInt(52)),
+//                    "drawable",
+//                    packageName
+//                )
+//            }
+//            binding.card1.setImageResource(res[0])
+//            binding.card2.setImageResource(res[1])
+//            binding.card3.setImageResource(res[2])
+//            binding.card4.setImageResource(res[3])
+//            binding.card5.setImageResource(res[4])
+//        }
 
     }
 
